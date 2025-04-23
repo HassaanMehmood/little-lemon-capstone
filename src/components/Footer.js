f0db6f2f18e864bf75ea4c3024fb4502ae04bead
HassaanMehmood <hassaanmahmood851@gmail.com>
2025-04-23 17:28:41 +0500

    Updated OG & Samentic Html

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Little Lemon. All rights reserved.</p>
+      <p>123 Citrus Street, Chicago, IL</p>
+    </footer>
+  );
+}
+
+export default Footer;
